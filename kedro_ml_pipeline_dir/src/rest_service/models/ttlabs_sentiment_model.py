9bb0f52dc020070bf4fee0a48fce988b0bba5f84
Akorlie Edward Tsatsu <65318539+edwardtsatsu@users.noreply.github.com>
2022-04-01 17:31:49 +0000

    updated the response

--- kedro_ml_pipeline_dir/src/rest_service/models/ttlabs_sentiment_model.py
+++ kedro_ml_pipeline_dir/src/rest_service/models/ttlabs_sentiment_model.py
@@ -3,10 +3,11 @@ from rest_service.models.enums.sentiment_label import SentimentLabel
 
 from typing import Dict, List
 
+
 class ModelPredictionRequest(BaseModel):
     text_sentiment: str
     ttlab_sentiment_type: List[SentimentLabel]
 
 
 class ModelPredictionResponse(BaseModel):
-    sentiment_response: Dict[SentimentLabel,float]
+    sentiment_response: Dict[SentimentLabel, float]
--- kedro_ml_pipeline_dir/src/rest_service/services/ttlabs_sentiment_service.py
+++ kedro_ml_pipeline_dir/src/rest_service/services/ttlabs_sentiment_service.py
@@ -3,7 +3,7 @@ from xml.sax.handler import property_interning_dict
 from rest_service.models.ttlabs_sentiment_model import ModelPredictionRequest
 from rest_service.models.ttlabs_sentiment_model import ModelPredictionResponse
 import numpy as np
-import  pandas as pd
+import pandas as pd
 import tensorflow as tf
 import re
 import os
@@ -11,31 +11,34 @@ from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 from kedro_pipeline.pipelines.data_processing.nodes import preprocess_data
 
-
-data1 = pd.read_csv("C:\\Users\\User\\Desktop\\MainProject\\ml-pipeline\\kedro_ml_pipeline_dir\\data\\01_raw\\Sentiment.csv")
+data1 = pd.read_csv(
+    "C:\\Users\\User\\PycharmProjects\\ml-pipeline\\kedro_ml_pipeline_dir\\src\\rest_service\\csv_dataset\\Sentiment"
+    ".csv")
 tokenizer = Tokenizer(num_words=2000, split=' ')
 tokenizer.fit_on_texts(data1['text'].values)
 
+
 def my_pipeline(text_sentiment):
-  text = text_sentiment.lower()
-  new_text = re.sub('[^a-zA-z0-9\s]','',text)
-  new_text = re.sub('rt','',new_text)
-  X = tokenizer.texts_to_sequences(pd.Series(new_text).values)
-  X = pad_sequences(X,maxlen=28)
-  return X
+    text = text_sentiment.lower()
+    new_text = re.sub('[^a-zA-z0-9\s]', '', text)
+    new_text = re.sub('rt', '', new_text)
+    X = tokenizer.texts_to_sequences(pd.Series(new_text).values)
+    X = pad_sequences(X, maxlen=28)
+    return X
 
 
 def predict(sentiment_request: ModelPredictionRequest):
-  response = {}
-  clean_text = my_pipeline(sentiment_request.text_sentiment)
-  loaded_model = tf.keras.models.load_model("C:\\Users\\User\\Desktop\\MainProject\\ml-pipeline\\kedro_ml_pipeline_dir\\src\\rest_service\\services\\sentiment.h5")
-  predictions = loaded_model.predict(clean_text)
-  pred = predictions.tolist()[0]
-  for value in sentiment_request.ttlab_sentiment_type:
-    if value == "POSITIVE":
-      response["POSITIVE"] = pred[2]
-    elif value == "NEUTRAL":
-      response["NEUTRAL"] = pred[1]
-    else:
-      response["NEGATIVE"] = pred[0]
-  return ModelPredictionResponse(sentiment_response = response)
+    response = {}
+    clean_text = my_pipeline(sentiment_request.text_sentiment)
+    loaded_model = tf.keras.models.load_model("C:\\Users\\User\\PycharmProjects\\ml-pipeline\\kedro_ml_pipeline_dir"
+                                              "\\src\\rest_service\\saved_model\\sentiment.h5")
+    predictions = loaded_model.predict(clean_text)
+    pred = predictions.tolist()[0]
+    for value in sentiment_request.ttlab_sentiment_type:
+        if value == "POSITIVE":
+            response["POSITIVE"] = pred[2]
+        elif value == "NEUTRAL":
+            response["NEUTRAL"] = pred[1]
+        else:
+            response["NEGATIVE"] = pred[0]
+    return ModelPredictionResponse(sentiment_response=response)
